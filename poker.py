cd01724ff391ba9e41eeec041ff24fe22c8a1cb9
Aziz Harrington <azizharrington@gmail.com>
2014-03-02 21:32:28 -0600

    all finished

--- poker.py
+++ poker.py
@@ -1,3 +1,12 @@
+import random
+
+
+mydeck = [r+s for r in '23456789TJQKA' for s in 'SHDC']
+
+
+def deal(numhands, n=5, deck=mydeck):
+	return [random.sample(mydeck, n) for x in xrange(0, numhands)]
+
 
 def poker(hands):
 	"Return a list of winning hands: poker([hand,...]) => [hand,...]"
@@ -124,3 +133,66 @@ def test():
 
 
 print test()
+
+
+hand_names = {0:'high card',1:'pair',2:'two pair',3:'3 of a kind',4:'straight',5:'flush',6:'full house',7:'4 of a kind',8:'straight flush'}
+
+def hand_percentages(n=700*1000):
+	"Sample n random hands and print a table of percentages for each type of hand."
+	counts = [0] * 9
+	for i in range(n/10):
+		for hand in deal(10):
+			ranking = hand_rank(hand)[0]
+			counts[ranking] += 1
+	for i in reversed(range(9)):
+		print "%14s: %6.3f %%" % (hand_names[i], 100.*counts[i]/n)
+
+
+# alternate implementation courtesy of peter norvig
+# beware crazy looking python!
+
+def alt_hand_rank(hand):
+	"Return a value indicating how hight the hand ranks."
+	# counts is the count of each rank; ranks lists corresponding ranks
+	# E.g. '7 T 7 9 7' => counts = (3, 1, 1); ranks = (7, 10, 9)
+	groups = group(['--23456789TJQKA'.index(r) for r,s in hand])
+	counts, ranks = unzip(groups)
+	if ranks == (14, 5, 4, 3, 2):
+		ranks = (5, 4, 3, 2, 1)
+	straight = len(ranks) == 5 and max(ranks)-min(ranks) == 4
+	flush = len(set([s for r,s in hand])) == 1
+	return (9 if (5,) == counts else
+			8 if straight and flush else
+			7 if (4, 1) == counts else
+			6 if (3, 2) == counts else
+			5 if flush else
+			4 if straight else
+			3 if (3, 1, 1) == counts else
+			2 if (2, 2, 1) == counts else
+			1 if (2, 1, 1, 1) == counts else
+			0), ranks
+
+
+def group(items):
+	"Return a list of [(count, x)...], highest count first, then highest x first."
+	groups = [(items.count(x), x) for x in set(items)]
+	return sorted(groups, reverse=True)
+
+
+def unzip(pairs): return zip(*pairs)
+
+
+def alt_alt_hand_rank(hand):
+	"Return a value indicating how high the hand ranks"
+	# counts is the count of each rank; ranks lists corresponding ranks
+	# E.g. '7 T 7 9 7' => counts = (3, 1, 1); ranks = (7, 10, 9)
+	groups = group(['--23456789TJQKA'.index(r) for r in r,s in hand])
+	counts, ranks = unzip(groups)
+	if ranks == (14, 5, 4, 3, 2):
+		ranks = (5, 4, 3, 2, 1)
+	straight = len(ranks) == 5 and max(ranks)-min(ranks) == 4
+	flush =len(set([s for r,s in hand])) == 1
+	return max(count_rankings[counts], 4*straight + 5*flush), ranks
+
+count_rankings = {(5, ):10, (4, 1):7, (3, 2):6, (3, 1, 1):3, (2, 2, 1):2,
+				  (2, 1, 1, 1):1, (1, 1, 1, 1, 1):0}
