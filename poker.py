7ca482e3057bf8214e093ac59a2e924863702371
azizh <azizh@zorotools-VirtualBox.(none)>
2014-03-04 09:48:27 -0600

    homework 1

--- poker.py
+++ poker.py
@@ -214,5 +214,36 @@ def test_best_hand():
 			== ['7C', '7D', '7H', '7S', 'JD'])
 	return 'test_best_hand passes'
 
+
 print test_best_hand()
 
+
+def best_wild_hand(hand):
+	"Try all values for jokers in all 5-card selections."
+	hands = set(best_hand(h)
+				for h in itertools.product(*map(replacements, hand)))
+	return max(hands, key=hand_rank)
+
+
+def replacements(card):
+	return (blackcards if card == '?B' else
+			redcards if card == '?R' else
+			[card])
+
+
+allranks = '23456789TJQKA'
+redcards = [r + s for r in allranks for s in 'DH']
+blackcards = [r + s for r in allranks for s in 'SC']
+
+
+def test_best_wild_hand():
+    assert (sorted(best_wild_hand("6C 7C 8C 9C TC 5C ?B".split()))
+            == ['7C', '8C', '9C', 'JC', 'TC'])
+    assert (sorted(best_wild_hand("TD TC 5H 5C 7C ?R ?B".split()))
+            == ['7C', 'TC', 'TD', 'TH', 'TS'])
+    assert (sorted(best_wild_hand("JD TC TH 7C 7D 7S 7H".split()))
+            == ['7C', '7D', '7H', '7S', 'JD'])
+    return 'test_best_wild_hand passes'
+
+
+print test_best_wild_hand()
