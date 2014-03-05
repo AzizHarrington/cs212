33624851418fb600c48a751821ef2747d093e7d7
Aziz Harrington <azizharrington@gmail.com>
2014-03-05 13:09:57 -0600

    refactor zebra puzzle, timedcall

--- zebrapuzzle.py
+++ zebrapuzzle.py
@@ -4,30 +4,30 @@ houses = [1, 2, 3, 4, 5]
 orderings = list(itertools.permutations(houses))
 
 
-def zebra_puzzle1():
+def zebra_puzzle():
     "Return a tuple (WATER, ZEBRA) indicating their house numbers."
     houses = first, _, middle, _, _ = [1, 2, 3, 4, 5]
     orderings = list(itertools.permutations(houses))
     return next((WATER, ZEBRA)
             for (red, green, ivory, yellow, blue) in orderings
+            if imright(green, ivory)
             for (Englishman, Spaniard, Ukrainian, Japanese, Norwegian) in orderings
-            for (dog, snails, fox, horse, ZEBRA) in orderings
-            for (coffee, tea, milk, oj, WATER) in orderings
-            for (OldGold, Kools, Chesterfields, LuckyStrike, Parliaments) in orderings
             if Englishman is red
+            if Norwegian is first
+            if nextto(Norwegian, blue)
+            for (dog, snails, fox, horse, ZEBRA) in orderings
             if Spaniard is dog
+            for (coffee, tea, milk, oj, WATER) in orderings
             if coffee is green
             if Ukrainian is tea
-            if imright(green, ivory)
+            if milk is middle
+            for (OldGold, Kools, Chesterfields, LuckyStrike, Parliaments) in orderings
             if OldGold is snails
             if Kools is yellow
-            if milk is middle
-            if Norwegian is first
-            if nextto(ChesterFields, fox)
+            if nextto(Chesterfields, fox)
             if nextto(Kools, horse)
             if LuckyStrike is oj
             if Japanese is Parliaments
-            if nextto(Norwegian, blue)
             )
 
 
@@ -39,3 +39,16 @@ def imright(h1, h2):
 def nextto(h1, h2):
     "Two houses are next to each other if they differ by 1."
     return abs(h1 - h2) == 1
+
+
+import time
+
+def timedcall(fn, *args):
+    "Call function and return elapsed time."
+    t0 = time.clock()
+    result = fn(*args)
+    t1 = time.clock()
+    return t1-t0, result
+
+
+print timedcall(zebra_puzzle)
