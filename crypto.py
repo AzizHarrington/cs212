e4661a237669cd0680582368c3208046f0f75f53
azizh <azizh@zorotools-VirtualBox.(none)>
2014-03-26 12:19:19 -0500

    added fill_in, solve functions to crypto

--- crypto.py
+++ crypto.py
@@ -1,39 +1,26 @@
-"""
-  ODD
-+ ODD
------
- EVEN
 
- solve for O, D, E, V, N
- """
-
-# permutations = 10! = 3mil
-# lets try brute force!
-
-"""
-Concept inventory:
-------------------
-Concept      Representation
-
-equations        
-    original ==> str
-    filled   ==> str
-letters      ==> 'D'
-digits       ==> '3'
-assignment   ==> table 'd' -> '3'  (see str.translate)
-    (letter to digit)
-evaluation  (see eval())
-"""
 
 import string, re
 
-# translate table example
-table = string.maketrans('ABC', '123')
 
-f = 'A + B == C'
+def solve(formula):
+    """Given a formula like 'ODD + ODD === EVEN', fill in digits to solve it.
+    Input formula is a string; output is a digit-filled-in string or None."""
+    return next(f for f in fill_in(formula) if valid(f))
+
+
+def fill_in(formula):
+    """Generate all possible fillings-in of letters in formula with digits."""
+    letters = ''.join(set([a for a in formula if a.isalpha()]))
+    for digits in itertools.permutations('1234567890', len(letters)):
+        table = string.maketrans(letters, ''.join(digits))
+        yield formula.translate(table)
+
 
 def valid(f):
     "Formula f is valid if it has no numbers with leading zero and evals true."
     try:
-
+        return not re.search(r'\b0[0-9]', f) and eval(f) is True
     except ZeroDivisionError:
+        return False
+
