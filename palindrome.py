51e408b291f95e5cb6884e4d2a5757ceafdaba72
azizh <azizh@zorotools-VirtualBox.(none)>
2014-04-07 14:58:01 -0500

    slow palindrome

--- palindrome.py
+++ palindrome.py
@@ -17,34 +17,35 @@
 
 def longest_subpalindrome_slice(text):
     "Return (i, j) such that text[i:j] is the longest palindrome in text."
-    current = {'l': 0, 'points': ()}
+    current = {'l': 0, 'points': (0, 0)}
     length = len(text)
     for i in range(length + 1):
         start = current['l']
-        for j in range(length + 1)[start:]:
-            a = text[i:j]
-            b = a[::-1]
-            if a == b:
-                current['l'] = len(a)
-                current['points'] = (i, j)
+        if i + start < length:
+            for j in range(length + 1)[start:]:
+                a = text[i:j]
+                b = a[::-1]
+                if a.upper() == b.upper() and len(a) > current['l']:
+                    current['l'] = len(a)
+                    current['points'] = (i, j)
     return current['points']
   
 
 L = longest_subpalindrome_slice
 
-# def test():
-#     assert L('racecar') == (0, 7)
-#     assert L('Racecar') == (0, 7)
-#     assert L('RacecarX') == (0, 7)
-#     assert L('Race carr') == (7, 9)
-#     assert L('') == (0, 0)
-#     assert L('something rac e car going') == (8,21)
-#     assert L('xxxxx') == (0, 5)
-#     assert L('Mad am I ma dam.') == (0, 15)
-#     return 'tests pass'
+def test():
+    assert L('racecar') == (0, 7)
+    assert L('Racecar') == (0, 7)
+    assert L('RacecarX') == (0, 7)
+    assert L('Race carr') == (7, 9)
+    assert L('') == (0, 0)
+    assert L('something rac e car going') == (8,21)
+    assert L('xxxxx') == (0, 5)
+    assert L('Mad am I ma dam.') == (0, 15)
+    return 'tests pass'
 
-# print test()
+print test()
 
 
 
-print L('aca')
+# print L('something rac e car going')
