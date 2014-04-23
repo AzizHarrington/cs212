bb262ea4ebcd3fe61f5d6b10ed07763f83d9f109
aziz <azizharrington@gmail.com>
2014-04-22 19:43:49 -0500

    finished palindrome

--- palindrome.py
+++ palindrome.py
@@ -15,7 +15,8 @@
 #
 # Please do not use regular expressions to solve this quiz!
 
-def longest_subpalindrome_slice(text):
+
+def my_longest_subpalindrome_slice(text): #not fast enough....
     "Return (i, j) such that text[i:j] is the longest palindrome in text."
     longest_palindrome = {'length': 0, 'points': (0, 0)}
     text_length = len(text)
@@ -32,9 +33,28 @@ def longest_subpalindrome_slice(text):
 
 def is_palindrome(string):
     return string.upper() == string.upper()[::-1]
-  
 
-L = longest_subpalindrome_slice
+
+def norvig_longest_subpalindrome_slice(text):
+    "Return (i, j) such that text[i:j] is the longest palindrome in text."
+    if text == '': return (0,0)
+    def length(slice): a,b = slice; return b-a
+    candidates = [grow(text, start, end)
+                  for start in range(len(text))
+                  for end in (start, start+1)]
+    return max(candidates, key=length)
+
+
+def norvig_grow(text, start, end):
+    "Start with a 0- or 1- length palindrome; try to grow a bigger one."
+    while (start > 0 and end < len(text)
+            and text[start-1].upper() == text[end].upper()):
+        start -= 1; end += 1
+    return(start, end)
+
+
+L = my_longest_subpalindrome_slice
+
 
 def test():
     assert L('racecar') == (0, 7)
